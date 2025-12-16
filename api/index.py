5948d7c50397c1d95224604f5f1011a1e5666a2d
Hritik Chauhan <hritikchji@gmail.com>
2025-12-16 17:18:39 +0530

    Enhance AI assistant functionality with structured planning and tool integration for weather queries

--- api/index.py
+++ api/index.py
@@ -2,12 +2,13 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import requests
+import json
+from pydantic import BaseModel, Field
+from typing import Optional
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
-)
+client = OpenAI()
 
 def get_weather(city: str):
     url = f"https://wttr.in/{city.lower()}?format=%C+%t"
@@ -17,17 +18,99 @@ def get_weather(city: str):
         return f"The current weather in {city} is: {response.text}"
     return "Something went wrong"
 
-def main():
-    user_query = input("Enter your query: ")
+available_tools = {
+    "get_weather": get_weather
+}
+
+SYSTEM_PROMPT = """
+    You're an expert AI Assistant in resolving user queries using chain of thought.
+    You work on START, PLAN and OUTPUT steps.
+    You need to first PLAN what needs to be done. The PLAN can be multiple steps.
+    Once you think enough PLAN has been done, finally you can give an OUTPUT.
+    You can also call a tool if required from the list of available tools.
+    for every tool call wait for the observe step which is the output from the called tool.
     
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": user_query}  
-        ]
-    )
+    Rules:
+    - Strickly Follow the given JSON output format
+    - Only run one step at a time.
+    - The sequence of steps is START (where user gives an input), PLAN (That can be multiple times)
+    and finally OUTPUT (which is going to the displayed to the user).
     
-    print("Response:", response.choices[0].message.content)
+    Output JSON Format:
+    { "step": "START" | "PLAN" | "TOOL" | "OBSERVE" | "OUTPUT", "content": "string" }
     
-# main()
-print(get_weather("New York"))
+    Available Tools:
+    - get_weather(city: str): Takes city name as input string and returns weather info about the city.
+    
+    Example 1:
+    START: { "step": "START", "content": "Hey, Can you solve 2 + 3 * 5 / 10"} 
+    PLAN: { "step": "PLAN", "content": "Seems like user is interested in math problem" }
+    PLAN: { "step": "PLAN", "content": "loolking at the problem, we should solve this using BODMAS method" }
+    PLAN: { "step": "PLAN", "content": "Yes, The BODMAS is correct thing to be done here" }
+    PLAN: { "step": "PLAN", "content": "first we must multiply 3 * 5 which is 15" }
+    PLAN: { "step": "PLAN", "content": "Now the new equation is 2 + 15 / 10" }
+    PLAN: { "step": "PLAN", "content": "We must perform divide that is 15 / 10 = 1.5" }
+    PLAN: { "step": "PLAN", "content": "Now the new equation is 2 + 1.5" }
+    PLAN: { "step": "PLAN", "content": "Now finally lets perform the add 3.5" }
+    PLAN: { "step": "PLAN", "content": "Great, we have solved and finally left with 3.5 as ans" }
+    OUTPUT: { "step": "OUTPUT", "content": "3.5" }
+    
+    Example 2:
+    START: { "step": "START", "content": "What is the weather of Delhi?"} 
+    PLAN: { "step": "PLAN", "content": "Seems like user is interested in getting weather of Delhi in India" }
+    PLAN: { "step": "PLAN", "content": "Lets see if we have any available tool the list of available tools" }
+    PLAN: { "step": "PLAN", "content": "Great, we have get_weather tool available for this query." }
+    PLAN: { "step": "PLAN", "content": "I need to call get_weather tool for delhi as input for city" }
+    PLAN: { "step": "TOOL", "tool": "get_weather", "input": "delhi" }
+    PLAN: { "step": "OBSERVE", "tool": "get_weather", "output": "The temp of delhi is cloudy with 20 C" }
+    PLAN: { "step": "PLAN", "content": "Great, I got the weather info about delhi" }
+    OUTPUT: { "step": "OUTPUT", "content": "The current weather in delhi is 20 C with some cloudy sky"}
+"""
+
+class MyOutputFormat(BaseModel):
+    step: str = Field(..., description="The ID of the step. Example: PLAN, OUTPUT, TOOL, etc")
+    content: Optional[str] = Field(None, description="The optional string content for the step")
+    tool: Optional[str] = Field(None, description="The ID of the tool to call")
+    input: Optional[str] = Field(None, description="The input params for the tool")
+
+message_history = [
+    { "role": "system", "content": SYSTEM_PROMPT },
+]
+
+while True:
+    user_query = input("👉 ")
+    message_history.append({"role": "user", "content": user_query})
+
+    while True:
+        response = client.chat.completions.parse(
+            model="gpt-4o-mini",
+            response_format=MyOutputFormat,
+            messages=message_history
+        )
+        
+        raw_result = response.choices[0].message.content
+        message_history.append({"role": "assistant", "content": raw_result})
+        
+        parsed_result = response.choices[0].message.parsed
+        
+        if parsed_result.step == "START":
+            print("🔥 ", parsed_result.content)
+            continue
+        
+        if parsed_result.step == "TOOL":
+            tool_to_call = parsed_result.tool
+            tool_input = parsed_result.input
+            print(f"🔧 Calling tool {tool_to_call} with input {tool_input}")
+            tool_response = available_tools[tool_to_call](tool_input)
+            message_history.append({"role": "developer", "content": json.dumps(
+                {"step": "OBSERVE", "tool": tool_to_call, "input": tool_input, "output": tool_response}
+            )})
+            continue
+            
+        if parsed_result.step == "PLAN":
+            print("🧠 ", parsed_result.content)
+            continue
+            
+        if parsed_result.step == "OUTPUT":
+            print("🤖 ", parsed_result.content)
+            break
