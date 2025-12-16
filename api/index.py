d6686b9a11f2a2c9dd675225a96e82a4ceaf3676
Hritik Chauhan <hritikchji@gmail.com>
2025-12-16 14:34:30 +0530

    Add weather fetching functionality and update requirements

--- api/index.py
+++ api/index.py
@@ -1,12 +1,21 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
 )
+
+def get_weather(city: str):
+    url = f"https://wttr.in/{city.lower()}?format=%C+%t"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return f"The current weather in {city} is: {response.text}"
+    return "Something went wrong"
 
 def main():
     user_query = input("Enter your query: ")
@@ -20,4 +29,5 @@ def main():
     
     print("Response:", response.choices[0].message.content)
     
-main()
+# main()
+print(get_weather("New York"))
